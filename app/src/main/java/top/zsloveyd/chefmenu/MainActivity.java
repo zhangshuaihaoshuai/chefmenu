87506a03ea8e7b5012dcdb9dfc24d33e7c77680c
zhangshuaihaoshuai <852575955@qq.com>
2020-04-29 11:05:04 +0800

    git 回滚

--- app/src/main/java/top/zsloveyd/chefmenu/MainActivity.java
+++ app/src/main/java/top/zsloveyd/chefmenu/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("git第一次提交");
+
+        System.out.println("回滚");
     }
 }
