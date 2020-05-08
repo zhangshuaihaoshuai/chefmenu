f0963211823de533ab39149c368ddcaac5581a85
zhangshuaihaoshuai <852575955@qq.com>
2020-05-08 16:51:09 +0800

    轮播图集成

---
+++ app/src/main/java/top/zsloveyd/chefmenu/adapter/ImageNetAdapter.java
@@ -0,0 +1,47 @@
+package top.zsloveyd.chefmenu.adapter;
+
+
+import android.os.Build;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.adapter.BannerAdapter;
+import com.youth.banner.util.BannerUtils;
+
+import java.util.List;
+
+import top.zsloveyd.chefmenu.R;
+import top.zsloveyd.chefmenu.model.DataBean;
+import top.zsloveyd.chefmenu.viewholder.ImageHolder;
+
+
+/**
+ * 自定义布局，网络图片
+ */
+public class ImageNetAdapter extends BannerAdapter<DataBean, ImageHolder> {
+
+    public ImageNetAdapter(List<DataBean> mDatas) {
+        super(mDatas);
+    }
+
+    @Override
+    public ImageHolder onCreateHolder(ViewGroup parent, int viewType) {
+        ImageView imageView = (ImageView) BannerUtils.getView(parent, R.layout.banner_image);
+        //通过裁剪实现圆角
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            BannerUtils.setBannerRound(imageView,20);
+        }
+        return new ImageHolder(imageView);
+    }
+
+    @Override
+    public void onBindView(ImageHolder holder, DataBean data, int position, int size) {
+        //通过图片加载器实现圆角，你也可以自己使用圆角的imageview，实现圆角的方法很多，自己尝试哈
+        Glide.with(holder.itemView)
+                .load(data.imageUrl)
+//                .apply(RequestOptions.bitmapTransform(new RoundedCorners(30)))
+                .into(holder.imageView);
+    }
+
+}
--- app/src/main/java/top/zsloveyd/chefmenu/fragment/HomeFragment.java
+++ app/src/main/java/top/zsloveyd/chefmenu/fragment/HomeFragment.java
@@ -9,10 +9,23 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.youth.banner.Banner;
+import com.youth.banner.indicator.CircleIndicator;
+import com.youth.banner.listener.OnPageChangeListener;
+import com.youth.banner.util.BannerUtils;
+
+import top.zsloveyd.chefmenu.R;
+import top.zsloveyd.chefmenu.adapter.ImageNetAdapter;
+import top.zsloveyd.chefmenu.model.DataBean;
+
 /**
  * Created by zhangxiaoshuai on 2020/5/8
  */
 public class HomeFragment extends BaseFragment {
+
+
+
 
 
     @Nullable
@@ -21,6 +34,52 @@ public class HomeFragment extends BaseFragment {
 
         View superView =  super.onCreateView(inflater, container, savedInstanceState);
         tvTitle.setText("首页");
+
+        View TextParentView = inflater.inflate(R.layout.fragment_home,container,false);
+        flContent.addView(TextParentView);
+
+
+
+
+        Banner banner = TextParentView.findViewById(R.id.banner);
+
+
+        //设置适配器
+        ImageNetAdapter adapter = new ImageNetAdapter(DataBean.getTestData3());
+        banner.setAdapter(adapter);
+        //设置指示器
+        banner.setIndicator(new CircleIndicator(mActivity));
+        //设置点击事件
+        banner.setOnBannerListener((data, position) -> {
+//            Snackbar.make(banner, ((DataBean) data).title, Snackbar.LENGTH_SHORT).show();
+        });
+        //添加切换监听
+        banner.addOnPageChangeListener(new OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        //圆角
+        banner.setBannerRound(BannerUtils.dp2px(5));
+
+
+
+
+
+
+
+
 
         return superView;
     }
